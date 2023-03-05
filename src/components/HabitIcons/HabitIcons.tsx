91dc3c34885515d65e73855c46f7d6542296a6ae
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-03-05 19:58:45 +0100

    chore(components): updated HabitIcons

--- src/components/HabitIcons/HabitIcons.tsx
+++ src/components/HabitIcons/HabitIcons.tsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { motion, LayoutGroup } from "framer-motion";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import cn from "classnames";
 
 import habitIcons from "@/constants/habitIcons";
 
@@ -13,48 +16,75 @@ interface IHabitIconsProps {
 
 const HabitIcons = ({ onHabitIconClick }: IHabitIconsProps): JSX.Element => {
   const [selectedHabitIcon, setSelectedHabitIcon] = useState("");
+  const [searchText, setSearchText] = useState("");
 
-  const habitClickHandler = (codeName: string) => {
+  const onHabitIconButtonClick = (codeName: string): void => {
     setSelectedHabitIcon(codeName);
+  };
+
+  const onSearchInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setSearchText(e.target.value.toLowerCase());
+  };
+
+  const onClearSearchInputButtonClick = (): void => {
+    setSearchText("");
   };
 
   return (
     <div className={styles.root}>
-      <LayoutGroup>
-        {habitIcons.map(({ id, iconUrl, codeName, iconAlt, iconName }, i) => {
-          return (
-            <motion.div
-              onClick={() => habitClickHandler(codeName)}
-              layout
-              key={id}
-              initial={{
-                x: -100,
-                scale: 0,
-                opacity: 0,
-              }}
-              animate={{
-                x: 0,
-                scale: 1,
-                opacity: 1,
-                transition: {
-                  duration: 0.5,
-                  delay: i * 0.1,
-                },
-              }}
-              style={{ display: "flex" }}
-            >
-              <HabitIcon
-                iconUrl={iconUrl}
-                codeName={codeName}
-                iconAlt={iconAlt}
-                iconName={iconName}
-                selected={selectedHabitIcon === codeName}
-                onClick={onHabitIconClick}
-              />
-            </motion.div>
-          );
-        })}
-      </LayoutGroup>
+      <div className={cn("p-inputgroup", styles.search)}>
+        <InputText
+          value={searchText}
+          onChange={onSearchInputChange}
+          placeholder="Search..."
+        />
+
+        <Button
+          onClick={onClearSearchInputButtonClick}
+          icon="pi pi-trash"
+          className="p-button-danger"
+        />
+      </div>
+
+      <div className={styles.icons}>
+        <LayoutGroup>
+          {habitIcons
+            .filter((hi) => hi.iconName.toLowerCase().includes(searchText))
+            .map(({ id, iconUrl, codeName, iconAlt, iconName }, i) => {
+              return (
+                <motion.div
+                  onClick={() => onHabitIconButtonClick(codeName)}
+                  layout
+                  key={id}
+                  initial={{
+                    x: -100,
+                    scale: 0,
+                    opacity: 0,
+                  }}
+                  animate={{
+                    x: 0,
+                    scale: 1,
+                    opacity: 1,
+                    transition: {
+                      duration: 0.5,
+                      delay: i * 0.1,
+                    },
+                  }}
+                  style={{ display: "flex" }}
+                >
+                  <HabitIcon
+                    iconUrl={iconUrl}
+                    codeName={codeName}
+                    iconAlt={iconAlt}
+                    iconName={iconName}
+                    selected={selectedHabitIcon === codeName}
+                    onClick={onHabitIconClick}
+                  />
+                </motion.div>
+              );
+            })}
+        </LayoutGroup>
+      </div>
     </div>
   );
 };
