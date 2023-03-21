439196584e774460d868ad2d27be5d8f8ddaf805
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-03-21 19:15:49 +0100

    chore(helpers): added JSDoc.

--- src/helpers/checkLimit.ts
+++ src/helpers/checkLimit.ts
@@ -1,3 +1,14 @@
+/**
+ * @param initialValue - initial value of string
+ * @type {string}
+ *
+ * @param target - target value to check
+ * @type {number}
+ *
+ * @returns true of false
+ * @type {boolean}
+ */
+
 const checkLimit = (initialValue: string, target: number): boolean => {
   if (initialValue.length >= target) {
     return true;
--- src/helpers/countProgress.ts
+++ src/helpers/countProgress.ts
@@ -1,3 +1,14 @@
+/**
+ * @param initialValue - initial value, the value has to be an array, otherwise it will return 0
+ * @type {string[] | number | undefined}
+ *
+ * @param target - target value to check
+ * @type {number}
+ *
+ * @returns value in percentage from 0 to 100
+ * @type {number}
+ */
+
 const countProgress = (
   initialValue: string[] | number | undefined,
   target: number
