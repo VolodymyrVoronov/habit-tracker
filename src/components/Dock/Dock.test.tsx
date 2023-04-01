0574fbd2c7278b836c0daa83c15dc6a48fc860b1
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-04-01 15:21:23 +0200

    test(Dock): tested component.

--- src/components/Dock/Dock.test.tsx
+++ src/components/Dock/Dock.test.tsx
@@ -1,11 +1,42 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import mockRouter from "next-router-mock";
+import { MemoryRouterProvider } from "next-router-mock/MemoryRouterProvider";
 
 import Dock from "./Dock";
 
-it("renders dock items, which has 4 links", async () => {
+it("renders dock items, which has 4 buttons", async () => {
   render(<Dock />);
 
-  const links = screen.getAllByRole("menuitem");
+  const buttons = screen.getAllByRole("menuitem");
 
-  expect(links).toHaveLength(4);
+  expect(buttons).toHaveLength(4);
+});
+
+it("leads to correct path/page by click on buttons", async () => {
+  render(<Dock />, { wrapper: MemoryRouterProvider });
+
+  const mainButton = screen.getByRole("menuitem", {
+    name: /icon main/i,
+  });
+  const habitsButton = screen.getByRole("menuitem", {
+    name: /icon habits/i,
+  });
+  const weatherButton = screen.getByRole("menuitem", {
+    name: /icon weather/i,
+  });
+  const radioButton = screen.getByRole("menuitem", {
+    name: /icon radio/i,
+  });
+
+  fireEvent.click(habitsButton);
+  expect(mockRouter.asPath).toEqual("/habits");
+
+  fireEvent.click(weatherButton);
+  expect(mockRouter.asPath).toEqual("/weather");
+
+  fireEvent.click(radioButton);
+  expect(mockRouter.asPath).toEqual("/radio");
+
+  fireEvent.click(mainButton);
+  expect(mockRouter.asPath).toEqual("/");
 });
